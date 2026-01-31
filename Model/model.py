11fc84bb167900b9c3042c63144823d42dd2b8ff
informatique <informatique@gpu-ihm.isir.lan>
2026-01-31 01:38:31 +0100

    python model containerized

--- Model/model.py
+++ Model/model.py
@@ -25,7 +25,8 @@ from transformers import AutoModelForSequenceClassification, AutoTokenizer
 import torch
 
 # Setup DataStore and Services
-store = DataStore("http://localhost:3030")
+store = DataStore("http://" + os.getenv("MARCELLE_BACKEND_SERVER", "localhost") + ":" + os.getenv("MARCELLE_BACKEND_PORT", "3030"))
+#store = DataStore("http://localhost:3030")
 #store = DataStore("https://marcelle.lisn.upsaclay.fr/nig-viz/api")
 
 ig_service = store.service("predictions")
