c2e144b0392c4c6da84abd9059e5cc1f8c733655
Loïc Charlery <charlery@isir.upmc.fr>
2026-01-31 04:09:40 +0100

    Generate the backend URL according to the frontend address

--- src/lib/marcelle/store.ts
+++ src/lib/marcelle/store.ts
@@ -5,7 +5,18 @@ import type { User } from '$lib/declarations';
 // Connect to Marcelle backend (MongoDB + SocketIO)
 // Make sure to run: pnpm backend
 export const user = writable<User | undefined>(undefined);
-export const store = dataStore('http://localhost:3030');
+//export const store = dataStore('http://localhost:3030');
+
+// Generate the backend URL according to the frontend address 
+let myprotocol = window.location.protocol;
+let myhostname = window.location.hostname;
+let backendUrl = '';
+if (myhostname === 'localhost' || myhostname === '127.0.0.1') {
+  backendUrl = myprotocol + '//' + myhostname + ':3030';
+} else {
+  backendUrl = myprotocol + '//' + myhostname + '/backend/';
+}
+export const store = dataStore(backendUrl);
 
 // Track if user is authenticated (not anonymous)
 export const isAuthenticated = writable<boolean>(false);
